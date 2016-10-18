7f2cd667fa960ec112379848f256ba6f524b6ce5
SebastianWenger <swenger@uncc.edu>
2016-10-18 15:34:20 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,82 +1,63 @@
 d1 <- "C:/Users/Sebastian/Documents/R Files/Getting and Cleaning Data/Week 4/UCI HAR Dataset/train"  
-
 d2 <- "C:/Users/Sebastian/Documents/R Files/Getting and Cleaning Data/Week 4/UCI HAR Dataset/test"
-
 d3 <- "C:/Users/Sebastian/Documents/R Files/Getting and Cleaning Data/Week 4/UCI HAR Dataset"
 
+#1: Merges training and test
 setwd(d1)
-      subject_train <- read.table("subject_train.txt")
-      y_train <- read.table("y_train.txt")
-      x_train <- read.table("x_train.txt")
+subject_train <- read.table("subject_train.txt",col.names=c("subject"))
+y_train <- read.table("y_train.txt", col.names=c("activity"))
+x_train <- read.table("x_train.txt")
 setwd(d2)
-      subject_test <- read.table("subject_test.txt")
-      y_test <- read.table("y_test.txt")
-      x_test <- read.table("x_test.txt")
-      
-#Merge training and test by row
-mergesubject <- rbind(subject_train, subject_test)
-mergey <- rbind(y_train, y_test)
-mergex <- rbind(x_train, x_test)
+subject_test <- read.table("subject_test.txt", col.names=c("subject"))
+y_test <- read.table("y_test.txt", col.names=c("activity"))
+x_test <- read.table("x_test.txt")
 
-#Merge the merged tables by column
-mergesubject <- cbind(mergesubject, mergey)
-mergex <- cbind(mergesubject, mergex)
-setNames(mergex, names(mergex) <- c("subject", "activity"))
+# Fill id column by row number
+subject_train$id <- as.numeric(rownames(subject_train))
+x_train$id <- as.numeric(rownames(x_train))
+y_train$id <- as.numeric(rownames(y_train))
+subject_test$id <- as.numeric(rownames(subject_test))
+x_test$id <- as.numeric(rownames(x_test))
+y_test$id <- as.numeric(rownames(y_test))
 
-#Dl mean and standard deviation data
+# merge subject_train, y_train, and x_train
+mergetrain <- merge(subject_train, y_train, all=TRUE)
+mergetrain <- merge(mergetrain, x_train, all=TRUE)
+# merge subject_test, y_test, and x_test 
+mergetest <- merge(subject_test, y_test, all=TRUE) 
+mergetest <- merge(mergetest, x_test, all=TRUE) 
+#combine train and test
+traintest <- rbind(mergetrain, mergetest)
+
+
+#2: Extracts mean and standard deviation of each measurement
 setwd(d3)
-      names <- read.table("names.txt")
-      colnames(names)[1:2] <-  c("number", "name")
+features = read.table("features.txt", col.names=c("number", "feature"))
+specific_features <- features$feature[grep("mean\\(\\)|(std\\(\\)", features$feature)]
+step2 <- traintest[, c(c(1, 2, 3), specific_features$number + 3) ]
 
-#Subset mean and standard deviation
+#3: Activities named descriptively
+activities = read.table("activity_labels.txt", col.names=c("activity", "activity_label"))
+step3 = merge(step2, activities)
 
-subsetnames <- names$name[grep("mean\\(\\)|std\\(\\)", names$name)]
-subsetnumbers <- names$number[grep("mean\\(\\)|std\\(\\)", names$name)]
-subsetnames <- as.character(subsetnames)
-subsetnumbers <- as.character(subsetnumbers)
-bind <- cbind(subsetnumbers, subsetnames)
-colnames(bind)[1:2] <- c("number", "name")
-bind <- as.data.frame(bind)
-bind$number <- paste("V", bind$number, sep="")
-bind$name <- as.character(bind$name)
-rownames(bind) <- bind[,1]
-names(mergex) <- paste("V", 1:ncol(mergex)-2, sep="")
-final <- subset(mergex,select=bind$number)
-transposefinal <- t(final)
-rownames(bind) <- bind[,1]
-final1 <- merge(bind, transposefinal, by="row.names")
+#4: Dataset labeled with descriptive variable names 
+specific_features$feature = gsub("\\(\\)", "", specific_features$feature)
+specific_features$feature = gsub("-", ".", specific_features$feature)
+for (i in 1:length(specific_features$feature)) {
+      colnames(step3)[i + 3] <- specific_features$feature[i]
+}
+step4 = step3
 
-#Creates descriptive activity names
-mergey[, 1] <- read.table("activity_labels.txt")[mergey[, 1], 2]
-names(mergey) <- "activity"
-
-
-#Appropriately labels data set with descriptive names
-names(mergesubject) <- "subject"
-names(final1) <- paste("V", 1:ncol(final1)-3, sep="")
-mergex$V0 <- factor(mergex$V0, levels=c('1','2','3','4','5','6'), 
-labels=c('WALKING', 'WALKING_UPSTAIRS', 'WALKING_DOWNSTAIRS', 'SITTING', 'STANDING', 'LAYING'))
-final1$V0 <- gsub("Acc","Acceleration", final1$V0)
-final1$V0 <- gsub("Mag","Magnitude", final1$V0)
-final1$V0 <- gsub("Gyro","AngularSpeed", final1$V0)
-final1$V0 <- gsub("Freq\\.","Frequency.", final1$V0)
-final1$V0 <- gsub("GyroJerk","AngularAcceleration", final1$V0)
-final1$V0 <- gsub("^t","TimeDomain.", final1$V0)
-final1$V0 <- gsub("^f","FrequencyDomain.", final1$V0)
-final1$V0 <- gsub("Freq$","Frequency", final1$V0)
-
-mergena <- merge(mergex, final1, by="row.names")
-single <- merge(final1$V0, mergena, by="row.names")
-single$Row.names <- NULL
-single$Row.names <- NULL
-colnames(single)[1:3] <- c("name", "subject", "activity")
-
-#2nd independent data set
-means <- aggregate(single, by=list(single$subject, single$activity), FUN=mean)
-means$subject <- NULL
-means$activity <- NULL
-colnames(means)[1:2] <- c("subject", "activity")
-write.table(means, file = "meandata.txt",row.name=FALSE)
+#5: Independent tidy data set with the average of each variable for each activity and subject
+remove <- c("id","activity_label")
+step5 <- step4[,!(names(step4) %in% remove)]
+meandata <-aggregate(step5, by=list(subject = step5$subject, activity = step5$activity), FUN=mean, na.rm=TRUE)
+remove <- c("subject","activity")
+meandata <- meandata[,!(names(meandata) %in% remove)]
+meandata = merge(meandata, activities)
+meandata$activity = NULL
+meandata <- meandata[c(67, 1, 2, 3:66)]
+write.csv(file="meandata.csv", x=meandata, row.names = FALSE)
 
 
 
